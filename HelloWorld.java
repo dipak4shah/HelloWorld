dbbbaf903d51ad505ae8f09a541ec930158d824b
dipak4shah <dipak4shah@yahoo.com>
2016-09-24 13:00:22 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package main.java;
+
+import main.java.DevalCode.StringSub;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		/*
+		StringSub stringSub = new StringSub();
+		String inString = "Test me";
+		String outString = stringSub.format(inString);
+		System.out.println("Output : " + outString);
+		*/
+	}
+
+}
